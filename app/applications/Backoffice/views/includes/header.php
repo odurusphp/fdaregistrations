893a31da78d802b679b6ec5b4d86ba345006226f
odurusphp <odurusphp@gmail.com>
2018-12-28 16:18:36 +0000

    backodffice template added

--- app/applications/Backoffice/views/includes/header.php
+++ app/applications/Backoffice/views/includes/header.php
@@ -1,53 +1,162 @@
-<?php
-
-// unpack our $data array
-foreach($data as $name => $value){
-    $$name = $value;
-}
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title><?php echo SITENAME  ?></title>
-<!-- Favicons -->
-<link rel="shortcut icon" href="img/favicon.ico">
-    <!-- Fontawesome css -->
-    <link rel="stylesheet" href="<?php echo URLROOT ?>/front/css/font-awesome.min.css">
-    <!-- Ionicons css -->
-    <link rel="stylesheet" href="<?php echo URLROOT ?>/front/css/ionicons.min.css">
-    <!-- Nice select css -->
-    <link rel="stylesheet" href="<?php echo URLROOT ?>/front/css/nice-select.css">
-    <!-- Jquery fancybox css -->
-    <link rel="stylesheet" href="<?php echo URLROOT ?>/front/css/jquery.fancybox.css">
-    <!-- Jquery ui price slider css -->
-    <link rel="stylesheet" href="<?php echo URLROOT ?>/front/css/jquery-ui.min.css">
-    <!-- Meanmenu css -->
-    <link rel="stylesheet" href="<?php echo URLROOT ?>/front/css/meanmenu.min.css">
-    <!-- Nivo slider css -->
-    <link rel="stylesheet" href="<?php echo URLROOT ?>/front/css/nivo-slider.css">
-    <!-- Owl carousel css -->
-    <link rel="stylesheet" href="<?php echo URLROOT ?>/front/css/owl.carousel.min.css">
-    <!-- Bootstrap css -->
-    <link rel="stylesheet" href="<?php echo URLROOT ?>/front/css/bootstrap.min.css">
-    <!-- Custom css -->
-    <link rel="stylesheet" href="<?php echo URLROOT ?>/front/css/default.css">
-    <!-- Main css -->
-    <link rel="stylesheet" href="<?php echo URLROOT ?>/front/css/style.css">
-    <!-- Responsive css -->
-    <link rel="stylesheet" href="<?php echo URLROOT ?>/front/css/responsive.css">
-
-    <!-- Modernizer js -->
-    <script src="<?php echo URLROOT ?>/front/js/vendor/modernizr-3.5.0.min.js"></script>
-   
-    <!-- /custom JS files to be developed and maintained -->
-
-    <?php
-    if (isset($basehref)){
-        echo "<base href='$basehref'>";
-    }
-    ?>
-
-</head>
+<header class="topbar-nav">
+ <nav class="navbar navbar-expand fixed-top bg-white">
+  <ul class="navbar-nav mr-auto align-items-center">
+    <li class="nav-item">
+      <a class="nav-link toggle-menu" href="javascript:void();">
+       <i class="icon-menu menu-icon"></i>
+     </a>
+    </li>
+    <li class="nav-item">
+      <form class="search-bar">
+        <input type="text" class="form-control" placeholder="Enter keywords">
+         <a href="javascript:void();"><i class="icon-magnifier"></i></a>
+      </form>
+    </li>
+  </ul>
+     
+  <ul class="navbar-nav align-items-center right-nav-link">
+    <li class="nav-item dropdown-lg">
+      <a class="nav-link dropdown-toggle dropdown-toggle-nocaret waves-effect" data-toggle="dropdown" href="javascript:void();">
+        <i class="icon-envelope-open"></i><span class="badge badge-danger badge-up">24</span></a>
+      <div class="dropdown-menu dropdown-menu-right">
+        <ul class="list-group list-group-flush">
+         <li class="list-group-item d-flex justify-content-between align-items-center">
+          You have 24 new messages
+          <span class="badge badge-danger">24</span>
+          </li>
+          <li class="list-group-item">
+          <a href="javaScript:void();">
+           <div class="media">
+             <div class="avatar"><img class="align-self-start mr-3" src="<?php echo URLROOT.'/backoffice/'?>/images/avatars/avatar-1.png" alt="user avatar"></div>
+            <div class="media-body">
+            <h6 class="mt-0 msg-title">Jhon Deo</h6>
+            <p class="msg-info">Lorem ipsum dolor sit amet...</p>
+            <small>Today, 4:10 PM</small>
+            </div>
+          </div>
+          </a>
+          </li>
+          <li class="list-group-item">
+          <a href="javaScript:void();">
+           <div class="media">
+             <div class="avatar"><img class="align-self-start mr-3" src="assets/images/avatars/avatar-2.png" alt="user avatar"></div>
+            <div class="media-body">
+            <h6 class="mt-0 msg-title">Sara Jen</h6>
+            <p class="msg-info">Lorem ipsum dolor sit amet...</p>
+            <small>Yesterday, 8:30 AM</small>
+            </div>
+          </div>
+          </a>
+          </li>
+          <li class="list-group-item">
+          <a href="javaScript:void();">
+           <div class="media">
+             <div class="avatar"><img class="align-self-start mr-3" src="assets/images/avatars/avatar-3.png" alt="user avatar"></div>
+            <div class="media-body">
+            <h6 class="mt-0 msg-title">Dannish Josh</h6>
+            <p class="msg-info">Lorem ipsum dolor sit amet...</p>
+             <small>5/11/2018, 2:50 PM</small>
+            </div>
+          </div>
+          </a>
+          </li>
+          <li class="list-group-item">
+          <a href="javaScript:void();">
+           <div class="media">
+             <div class="avatar"><img class="align-self-start mr-3" src="assets/images/avatars/avatar-4.png" alt="user avatar"></div>
+            <div class="media-body">
+            <h6 class="mt-0 msg-title">Katrina Mccoy</h6>
+            <p class="msg-info">Lorem ipsum dolor sit amet.</p>
+            <small>1/11/2018, 2:50 PM</small>
+            </div>
+          </div>
+          </a>
+          </li>
+          <li class="list-group-item"><a href="javaScript:void();">See All Messages</a></li>
+        </ul>
+        </div>
+    </li>
+    <li class="nav-item dropdown-lg">
+      <a class="nav-link dropdown-toggle dropdown-toggle-nocaret waves-effect" data-toggle="dropdown" href="javascript:void();">
+      <i class="icon-bell"></i><span class="badge badge-primary badge-up">10</span></a>
+      <div class="dropdown-menu dropdown-menu-right">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item d-flex justify-content-between align-items-center">
+          You have 10 Notifications
+          <span class="badge badge-primary">10</span>
+          </li>
+          <li class="list-group-item">
+          <a href="javaScript:void();">
+           <div class="media">
+             <i class="icon-people fa-2x mr-3 text-info"></i>
+            <div class="media-body">
+            <h6 class="mt-0 msg-title">New Registered Users</h6>
+            <p class="msg-info">Lorem ipsum dolor sit amet...</p>
+            </div>
+          </div>
+          </a>
+          </li>
+          <li class="list-group-item">
+          <a href="javaScript:void();">
+           <div class="media">
+             <i class="icon-cup fa-2x mr-3 text-warning"></i>
+            <div class="media-body">
+            <h6 class="mt-0 msg-title">New Received Orders</h6>
+            <p class="msg-info">Lorem ipsum dolor sit amet...</p>
+            </div>
+          </div>
+          </a>
+          </li>
+          <li class="list-group-item">
+          <a href="javaScript:void();">
+           <div class="media">
+             <i class="icon-bell fa-2x mr-3 text-danger"></i>
+            <div class="media-body">
+            <h6 class="mt-0 msg-title">New Updates</h6>
+            <p class="msg-info">Lorem ipsum dolor sit amet...</p>
+            </div>
+          </div>
+          </a>
+          </li>
+          <li class="list-group-item"><a href="javaScript:void();">See All Notifications</a></li>
+        </ul>
+      </div>
+    </li>
+    <li class="nav-item language">
+      <a class="nav-link dropdown-toggle dropdown-toggle-nocaret waves-effect" data-toggle="dropdown" href="#"><i class="flag-icon flag-icon-gb"></i></a>
+      <ul class="dropdown-menu dropdown-menu-right">
+          <li class="dropdown-item"> <i class="flag-icon flag-icon-gb mr-2"></i> English</li>
+          <li class="dropdown-item"> <i class="flag-icon flag-icon-fr mr-2"></i> French</li>
+          <li class="dropdown-item"> <i class="flag-icon flag-icon-cn mr-2"></i> Chinese</li>
+          <li class="dropdown-item"> <i class="flag-icon flag-icon-de mr-2"></i> German</li>
+        </ul>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link dropdown-toggle dropdown-toggle-nocaret" data-toggle="dropdown" href="#">
+        <span class="user-profile"><img src="assets/images/avatars/avatar-17.png" class="img-circle" alt="user avatar"></span>
+      </a>
+      <ul class="dropdown-menu dropdown-menu-right">
+       <li class="dropdown-item user-details">
+        <a href="javaScript:void();">
+           <div class="media">
+             <div class="avatar"><img class="align-self-start mr-3" src="assets/images/avatars/avatar-17.png" alt="user avatar"></div>
+            <div class="media-body">
+            <h6 class="mt-2 user-title">Katrina Mccoy</h6>
+            <p class="user-subtitle">mccoy@example.com</p>
+            </div>
+           </div>
+          </a>
+        </li>
+        <li class="dropdown-divider"></li>
+        <li class="dropdown-item"><i class="icon-envelope mr-2"></i> Inbox</li>
+        <li class="dropdown-divider"></li>
+        <li class="dropdown-item"><i class="icon-wallet mr-2"></i> Account</li>
+        <li class="dropdown-divider"></li>
+        <li class="dropdown-item"><i class="icon-settings mr-2"></i> Setting</li>
+        <li class="dropdown-divider"></li>
+        <li class="dropdown-item"><i class="icon-power mr-2"></i> Logout</li>
+      </ul>
+    </li>
+  </ul>
+</nav>
+</header>
